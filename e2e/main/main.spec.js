b23c5ec14ccee2c344b50fb21a6c5a5e9a10c25d
haiku <haiku@node.in.ua>
2015-03-11 11:02:10 +0300

    updated hover and click test for comments

--- e2e/main/main.spec.js
+++ e2e/main/main.spec.js
@@ -5,6 +5,7 @@ describe('Main View', function() {
 
   beforeEach(function(done) {
     browser.get('/');
+    browser.waitForAngular();
 
     setTimeout(function () {
       page = require('./main.po');
@@ -16,4 +17,24 @@ describe('Main View', function() {
     expect(page.programs.count()).toBe(3);
     expect(page.discussions.count()).toBe(3);
   });
+
+  it ('should focus to comment section on comment click', function(done) {
+    var row = page.programs.get(1);
+
+    browser.driver
+        .actions()
+        .mouseMove(row)
+        .perform();
+
+    var discussButton = row.element(by.css('.discuss'));
+    expect(discussButton.getAttribute('title')).toBe('Обсудить');
+
+    discussButton.click();
+
+    setTimeout(function () {
+      var textarea = element(by.css('.add-discussion textarea'));
+      expect(textarea).toEqual(browser.driver.switchTo().activeElement());
+      done();
+    }, 1000);
+  })
 });
